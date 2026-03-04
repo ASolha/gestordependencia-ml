67c00cdf34a3878fae4359eed91c08a8725254e8
solha <alcsolha@gmail.com>
2026-03-04 11:43:49 -0300

    Flatten repository to extension root

--- as-sentinela-updater/SelfUpdateOptions.cs
+++
@@ -1,9 +0,0 @@
-namespace ASSentinela.Updater;
-
-internal sealed class SelfUpdateOptions
-{
-    public bool Enabled { get; set; } = true;
-    public string ReleaseMetadataUrl { get; set; } = "";
-    public string BootstrapExecutableName { get; set; } = "ASSentinela.Updater.Bootstrap.exe";
-    public string MainExecutableName { get; set; } = "ASSentinela.Updater.exe";
-}
--- as-sentinela-updater/SelfUpdateRelease.cs
+++
@@ -1,8 +0,0 @@
-namespace ASSentinela.Updater;
-
-internal sealed class SelfUpdateRelease
-{
-    public string Version { get; set; } = "";
-    public string PackageUrl { get; set; } = "";
-    public string Notes { get; set; } = "";
-}
